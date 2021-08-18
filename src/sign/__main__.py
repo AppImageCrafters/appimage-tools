18f4acec4f068e5afbfc2aa7f7f11c7c77aded24
Alexis Lopez Zubieta <contact@azubieta.net>
2021-08-18 14:09:04 -0500

    Add make verify a subcommand

--- src/sign/__main__.py
+++ src/sign/__main__.py
@@ -5,10 +5,12 @@ from sign.verify import verify_signature
 
 def main():
     parser = argparse.ArgumentParser(description="AppImage Signatures management tool")
+    subparsers = parser.add_subparsers(help='sub-command help')
 
-    parser.add_argument("target", help="AppImage file")
+    parser_verify = subparsers.add_parser('verify', help='verify help')
+    parser_verify.add_argument("target", help="AppImage file")
+
     args = parser.parse_args()
-
     verify_signature(args.target)
 
 
